3878b07b7e96e97413d6705fe9101f4c83831701
ketsi123 <ketsirin.noi@gmail.com>
2026-08-26 11:44:51 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,12 +1,8 @@
 window.FS_CONFIG = {
   BACKEND_URL: "https://script.google.com/macros/s/AKfycbzgeudLE5_yzFRQ4ejGE2_2aYwBIDSkxQRy2Ptj72b755Zy8ypvmcWlNR1cuLQRPKblpw/exec",
-
-  // ตอนนี้มีภาพจริงใน GitHub 2 ข้อเท่านั้น
-  // เมื่อทำ S1Q03 เสร็จ ให้เพิ่ม "S1Q03" ในรายการนี้แล้ว Upload assets/questions/S1_Q03.jpg
   ENABLED_QUESTION_IDS: ["S1Q01", "S1Q02"],
-
   QUESTION_IMAGE_DIR: "",
-  ASSET_VERSION: "20260826-V12-NEWBACKEND",
+  ASSET_VERSION: "20260826-1135",
   GAME_MODE: "NONCONFORMITY",
   RANDOMIZE_QUESTIONS: false,
   AUTO_NEXT_SECONDS: 0,
